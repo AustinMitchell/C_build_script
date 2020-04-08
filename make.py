e71fca4bb02e4b4d6b91a622e7b052e5ecb22073
Austin <austin.mitchell@gmail.com>
2020-04-08 15:13:44 -0400

    Fixed make script, added gitignore

--- make.py
+++ make.py
@@ -1,5 +1,5 @@
 from subprocess import Popen, PIPE, STDOUT
-from typing import List, Tuple, Any, Generator, Iterator
+from typing import List, Tuple, Any, Generator, Iterator, Set, Dict
 from pathlib import Path
 import shutil
 import os
@@ -7,6 +7,7 @@ import sys
 import yaml
 import io
 import argparse
+import re
 
 
 DEFAULT_CONFIG = """\
@@ -16,6 +17,7 @@ FLAGS: "-std=c++17 -Wall -Wextra"
 EXE_DIR: "./bin"
 EXE_FILE: "a.out"
 
+SOURCE_MAIN: "main.cc"
 SOURCE_DIR: "./src/"
 SOURCE_EXT: "cc"
 HEADER_DIR: "./include/"
@@ -32,6 +34,8 @@ class config:
 
     EXE_DIR: str
     EXE_FILE: str
+
+    SOURCE_MAIN: str
 
     SOURCE_DIR: str
     SOURCE_EXT: str
@@ -56,6 +60,8 @@ class config:
 
         config.EXE_DIR  = config_file["EXE_DIR"]
         config.EXE_FILE = config_file["EXE_FILE"]
+
+        config.SOURCE_MAIN = config_file["SOURCE_MAIN"]
 
         config.SOURCE_DIR = config_file["SOURCE_DIR"]
         config.SOURCE_EXT = config_file["SOURCE_EXT"]
@@ -107,39 +113,87 @@ def colour_print(message: str,
     """
     resetColor = styles.END.val if reset else ''
     print(f"{colour.val}{style.val}{message}{resetColor}", **kwargs)
-        
+
 
 
 def shell(cmd: str, stdout=None) -> Popen:
     """
-    Executes a command on the shell using Popen and returns the object created 
+    Executes a command on the shell using Popen and returns the object created
     """
 
     return Popen(cmd, shell=True, stdin=PIPE, stdout=stdout, stderr=None)
 
 
-def source_files() -> Generator[Path, None, None]:
+def test_source(source:Path, dependencies:List[Path]) -> bool:
     """
-    Creates a generator of all files in the project with the source extension set in the config.
+    Checks to see if source file needs to be built
     """
-    return Path(config.SOURCE_DIR).rglob(f"*.{config.SOURCE_EXT}")
 
+    # constructs object file name from source file name
+    objectfile = source_to_object(source)
+
+    build_required = False
+    source_mtime = source.stat().st_mtime
+
+    if os.path.exists(objectfile):
+        object_mtime = objectfile.stat().st_mtime
+        if object_mtime < source_mtime:
+            # Source is newer than object file, compile
+            build_required = True
+        else:
+            # Check if any dependencies are newer than object file
+            for dep in dependencies:
+                if object_mtime < dep.stat().st_mtime:
+                    # Dependency is newer than object file, compile
+                    build_required = True
+                    break
+    else:
+        # Objectfile hasnt been made, we need to compile
+        build_required = True
+
+    return build_required
+
+def source_files() -> List[Tuple[Path, bool]]:
+    """
+    Creates a generator of all files in the project with the source extension set in the config, and detemines if they need building
+    """
+    init_source:  Path                    = Path(config.SOURCE_DIR, config.SOURCE_MAIN)
+    deps:         List[Path]              = list(generate_dependencies(init_source))
+    sources:      List[Tuple[Path, bool]] = [(init_source, test_source(init_source, deps))]
+    checked_deps: Set[Path]               = set()
+
+    while(deps):
+        current_dep = deps.pop(0)
+        if current_dep in checked_deps:
+            continue
+
+        checked_deps.add(current_dep)
+        current_source = header_to_source(current_dep)
+
+        if os.path.exists(current_source):
+            new_deps = generate_dependencies(current_source)
+            sources.append((current_source, test_source(current_source, list(new_deps))))
+            deps.extend(new_deps)
+
+    return sources
+
+
+def header_to_source(header:Path) -> Path:
+    return Path(config.SOURCE_DIR).joinpath(Path(header).relative_to(config.HEADER_DIR).parent, f"{Path(header).stem}.{config.SOURCE_EXT}")
 
 def source_to_object(source:Path) -> Path:
     return Path(config.OBJECT_DIR).joinpath(Path(source).relative_to(config.SOURCE_DIR).parent, f"{Path(source).stem}.{config.OBJECT_EXT}")
 
-def dependencies(file: Path) -> Generator[Path, None, None]:
+def generate_dependencies(file: Path) -> Generator[Path, None, None]:
     """
     Generates a list of non-system dependencies to a source file using -MM
     """
 
     # This will create a string of all the non-system dependencies for our source file separated by spaces
     cmd = f"{config.COMPILER} {config.FLAGS} -MM -I{config.HEADER_DIR} {file}"
-    deps = str(shell(cmd, stdout=PIPE).stdout.read()).split(" ")
+    deps = re.findall(r"\S+\.hpp", str(shell(cmd, stdout=PIPE).stdout.read()))
 
-    del deps[:2] # first two outputs will be the name of the .o file that would be made and the name of the source file. Delete them.
-
-    return (Path(dep) for dep in deps if Path(dep).suffix == config.HEADER_EXT)
+    return (Path(dep) for dep in deps)
 
 
 def build():
@@ -153,11 +207,11 @@ def build():
 
     print("")
     print(f"Sources: {' '.join(str(s) for s in source_files())}")
-    print(f"Objects: {' '.join(str(source_to_object(s)) for s in source_files())}")
+    print(f"Objects: {' '.join(str(source_to_object(s)) for (s,b) in source_files())}")
     print("")
 
-    for source in source_files():
-        if test_source(source):
+    for (source, needs_building) in source_files():
+        if needs_building:
             if build_object(source):
                 linking_required = True
             else:
@@ -173,7 +227,7 @@ def build():
     else:
         print("")
         exe_full_path = Path(config.EXE_DIR).joinpath(config.EXE_FILE)
-        if not os.path.exists(exe_full_path):
+        if not linking_required and not os.path.exists(exe_full_path):
             linking_required = True
             colour_print(f"The file {exe_full_path} doesn't exist.", colour=colours.MGT, style=styles.BLD)
 
@@ -181,7 +235,7 @@ def build():
             # Build exe location folders
             Path(Path(config.EXE_DIR)).mkdir(parents=True, exist_ok=True)
 
-            cmd = (f"{config.COMPILER} {config.FLAGS} -o {exe_full_path} {' '.join((str(source_to_object(s)) for s in source_files()))}")
+            cmd = (f"{config.COMPILER} {config.FLAGS} -o {exe_full_path} {' '.join((str(source_to_object(s)) for (s,b) in source_files()))}")
 
             colour_print("Generating executable... ", colour=colours.CYN, style=styles.BLD)
             colour_print("Running: ", colour=colours.CYN, style=styles.BLD, end='')
@@ -225,35 +279,6 @@ def build_object(source_file:Path) -> int:
 
     return ret.returncode == 0
 
-
-def test_source(source:Path) -> bool:
-    """
-    Checks to see if source file needs to be built
-    """
-
-    # constructs object file name from source file name
-    objectfile = source_to_object(source)
-
-    build_required = False
-    source_mtime = source.stat().st_mtime
-
-    if os.path.exists(objectfile):
-        object_mtime = objectfile.stat().st_mtime
-        if object_mtime < source_mtime:
-            # Source is newer than object file, compile
-            build_required = True
-        else:
-            # Check if any dependencies are newer than object file
-            for dep in dependencies(source):
-                if object_mtime < os.path.getmtime(dep):
-                    # Dependency is newer than object file, compile
-                    build_required = True
-                    break
-    else:
-        # Objectfile hasnt been made, we need to compile
-        build_required = True
-
-    return build_required
 
 if __name__ == "__main__":
 
@@ -320,8 +345,8 @@ if __name__ == "__main__":
         if os.path.exists(config.OBJECT_DIR):
             colour_print("Removing " + config.OBJECT_DIR + "...", colour=colours.MGT)
             shutil.rmtree(config.OBJECT_DIR)
-        if os.path.exists(config.EXE_FILE):
+        if os.path.exists(exe_full_path):
             colour_print(f"Removing {exe_full_path}...",   colour=colours.MGT)
             os.remove(exe_full_path)
-    
+
     print("")
